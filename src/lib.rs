5baf0b31ff3c7ab7946dd9e734ff6ade11b634f7
Surya Majhi <suryamajhi20@gmail.com>
2024-09-19 18:23:22 +0545

    Test files and Exception setup initialized

---
+++ src/lib.rs
@@ -0,0 +1,66 @@
+mod token;
+
+use crate::token::Token;
+use std::{fs, io, process};
+
+static mut HAD_RUNTIME_ERROR: bool = false;
+
+struct RuntimeError {
+    token: Token,
+    message: String,
+}
+pub enum Exception {
+    RuntimeError(RuntimeError),
+}
+
+impl Exception {
+    fn runtime_error<T>(token: Token, message: String) -> Result<T, Exception> {
+        Err(Exception::RuntimeError(RuntimeError { token, message }))
+    }
+}
+
+fn check_runtime_error() {
+    unsafe {
+        if HAD_RUNTIME_ERROR {
+            process::exit(70)
+        }
+    }
+}
+
+pub fn print_error(line: usize, location: String, message: &str) {
+    eprintln!("[line {line}] Error at '{location}': {message}");
+    unsafe { HAD_RUNTIME_ERROR = true }
+}
+
+pub fn run_prompt() {
+    loop {
+        println!("> ");
+        let mut user_input = String::new();
+        io::stdin()
+            .read_line(&mut user_input)
+            .expect("Valid user input");
+
+        let user_input = user_input.trim();
+        if user_input == "exit" {
+            break;
+        }
+        run(user_input.to_string());
+        unsafe {
+            HAD_RUNTIME_ERROR = false;
+        }
+    }
+}
+
+pub fn run_file(path: &str) {
+    let file_contents = fs::read_to_string(path).expect("Could not read file");
+    run(file_contents);
+    unsafe {
+        if HAD_RUNTIME_ERROR {
+            process::exit(70);
+        }
+    }
+}
+
+fn run(source: String) {
+    println!("Run");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use rlox::{run_file, run_prompt};
+use std::cmp::Ordering;
+use std::{env, process};
+
 fn main() {
-    println!("Hello, world!");
+    // env::set_var("RUST_BACKTRACE", "1");
+
+    let args: Vec<String> = env::args().collect();
+
+    match args.len().cmp(&2) {
+        Ordering::Greater => {
+            println!("Usage: rlox [script]");
+            process::exit(64);
+        }
+        Ordering::Equal => run_file(&args[1]),
+        _ => {
+            run_prompt();
+        }
+    }
 }
